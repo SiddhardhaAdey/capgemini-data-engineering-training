db5c4d561432b50c8d612796b4cfe1d115bce4b3
SiddhardhaAdey <160423143+SiddhardhaAdey@users.noreply.github.com>
2026-03-29 22:40:56 +0530

    Update pyspark_code.py

--- week0/phase1/pyspark_code.py
+++ week0/phase1/pyspark_code.py
@@ -1,1 +1,20 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import count
 
+spark = SparkSession.builder.appName("Phase1").getOrCreate()
+
+data = [
+    (1, "Ravi", "Hyderabad", 25),
+    (2, "Sita", "Chennai", 32),
+    (3, "Arun", "Hyderabad", 28),
+    (4, "Meena", "Bangalore", 35),
+    (5, "Kiran", "Chennai", 22)
+]
+
+customers = spark.createDataFrame(data, ["id","name","city","age"])
+
+customers.show()
+customers.filter(customers.city == "Chennai").show()
+customers.filter(customers.age > 25).show()
+customers.select("name","city").show()
+customers.groupBy("city").agg(count("*")).show()

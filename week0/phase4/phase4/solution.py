cedd2ec1c61e57b8a2a81254101b80ea127db2a4
SiddhardhaAdey <160423143+SiddhardhaAdey@users.noreply.github.com>
2026-04-05 11:48:01 +0530

    Update solution.py

--- week0/phase4/phase4/solution.py
+++ week0/phase4/phase4/solution.py
@@ -3,97 +3,87 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, sum, count, when, concat_ws
 
 # ---------- SPARK SESSION ----------
-spark = SparkSession.builder.appName("phase4_etl_pipeline").getOrCreate()
+spark = SparkSession.builder.appName("phase4_mini_project").getOrCreate()
 
 # ---------- LOAD DATA ----------
-sales_data = spark.read.csv('/samples/sales.csv', header=True, inferSchema=True)
-customer_data = spark.read.csv('/samples/customers.csv', header=True, inferSchema=True)
+sales_df = spark.read.csv('/samples/sales.csv', header=True, inferSchema=True)
+customers_df = spark.read.csv('/samples/customers.csv', header=True, inferSchema=True)
 
 # ---------- DATA CLEANING ----------
-sales_data = sales_data.dropna(subset=["customer_id"])
-customer_data = customer_data.dropna(subset=["customer_id"])
+sales_df = sales_df.dropna(subset=["customer_id"])
+customers_df = customers_df.dropna(subset=["customer_id"])
 
-sales_data = sales_data.dropDuplicates()
-customer_data = customer_data.dropDuplicates()
+sales_df = sales_df.dropDuplicates()
+customers_df = customers_df.dropDuplicates()
 
-sales_data = sales_data.filter(col("total_amount") > 0)
+sales_df = sales_df.filter(col("total_amount") > 0)
+
 
 # =================================================
 # 1. DAILY SALES
 # =================================================
-daily_sales_df = sales_data.groupBy("sale_date") \
-    .agg(sum("total_amount").alias("total_sales"))
+daily_sales = sales_df.groupBy("sale_date").agg(sum("total_amount").alias("total_sales"))
 
 print("========= DAILY SALES =========")
-daily_sales_df.show()
+daily_sales.show()
+
 
 # =================================================
 # 2. CITY-WISE REVENUE
 # =================================================
-combined_df = customer_data.join(sales_data, "customer_id")
+data = customers_df.join(sales_df, "customer_id")
 
-city_revenue_df = combined_df.groupBy("city") \
-    .agg(sum("total_amount").alias("total_revenue"))
-
+city_wise_revenue = data.groupBy("city").agg(sum("total_amount").alias("revenue"))
 print("========= CITY REVENUE =========")
-city_revenue_df.show()
+city_wise_revenue.show()
+
 
 # =================================================
 # 3. TOP 5 CUSTOMERS
 # =================================================
-top_customers_df = combined_df.groupBy("customer_id") \
-    .agg(sum("total_amount").alias("total_spending")) \
-    .orderBy(col("total_spending").desc()) \
-    .limit(5)
+top_customers = data.groupBy("customer_id").agg(sum("total_amount").alias("total_spend")).orderBy(col("total_spend").desc()).limit(5)
 
 print("========= TOP 5 CUSTOMERS =========")
-top_customers_df.show()
+top_customers.show()
 
 # =================================================
 # 4. REPEAT CUSTOMERS (>1 ORDER)
 # =================================================
-repeat_customers_df = sales_data.groupBy("customer_id") \
-    .agg(count("*").alias("order_count")) \
-    .filter(col("order_count") > 1)
+repeat_customers = sales_df.groupBy("customer_id").agg(count("*").alias("order_count")).filter(col("order_count") > 1)
 
 print("========= REPEAT CUSTOMERS =========")
-repeat_customers_df.show()
+repeat_customers.show()
 
 # =================================================
 # 5. CUSTOMER SEGMENTATION
 # =================================================
-customer_spending_df = combined_df.groupBy("customer_id") \
-    .agg(sum("total_amount").alias("total_spending"))
+customer_spend = data.groupBy("customer_id") \
+    .agg(sum("total_amount").alias("total_spend"))
 
-customer_segment_df = customer_spending_df.withColumn(
+customer_segmentation = customer_spend.withColumn(
     "segment",
-    when(col("total_spending") > 10000, "Gold")
-    .when((col("total_spending") >= 5000) & (col("total_spending") <= 10000), "Silver")
+    when(col("total_spend") > 10000, "Gold")
+    .when((col("total_spend") >= 5000) & (col("total_spend") <= 10000), "Silver")
     .otherwise("Bronze")
 )
 
 print("========= CUSTOMER SEGMENTATION =========")
-customer_segment_df.show()
+customer_segmentation.show()
 
 # =================================================
 # 6. FINAL REPORT
 # =================================================
-customer_name_df = customer_data.withColumn(
-    "full_name",
+customer_name = customers_df.withColumn(
+    "customer_name",
     concat_ws(" ", col("first_name"), col("last_name"))
 )
 
-final_report_df = customer_name_df \
-    .join(customer_segment_df, "customer_id", "left") \
-    .join(repeat_customers_df, "customer_id", "left") \
-    .select("full_name", "city", "total_spending", "order_count", "segment")
+final_table = customer_name.join(customer_segmentation, "customer_id", "left").join(repeat_customers, "customer_id", "left").select("customer_name", "city", "total_spend", "order_count", "segment")
 
 print("========= FINAL REPORT =========")
-final_report_df.show()
+final_table.show()
 
 # =================================================
 # 7. SAVE OUTPUT
 # =================================================
-final_report_df.write.mode("overwrite") \
-    .option("header", "true") \
-    .csv('/tmp/output/report')
+final_table.write.mode("overwrite").option("header", "true").csv('/tmp/output/report')
